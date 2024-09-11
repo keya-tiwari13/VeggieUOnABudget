8f7d305039128e1561428f917269250c47d8e561
Keya Tiwari <k6tiwari@uwaterloo.ca>
2024-09-11 15:46:49 -0400

    fixing inability to display pictures bug

--- src/components/recipecard.js
+++ src/components/recipecard.js
@@ -1,16 +1,19 @@
 import CustomImage from "./customimage"
 
-export default function RecipeCard(recipe){
-    return(
+export default function RecipeCard({ recipe }) {
+    return (
         <div className="recipe-card">
-            <CustomImage imgSrc={recipe.image} pt="65%"/>
+            <CustomImage imgSrc={recipe.image} pt="65%" />
             <div className="recipe-card-info">
                 <p className="recipe-title">{recipe.title}</p>
-                <p className="recipe-info">super easy and quick muffins!</p> 
+                <p className="recipe-info">{recipe.info}</p> 
                 <a className="view-btn" href="#!">go!</a>
             </div>
         </div>
-    )
+    );
 }
 
+
 //<img className="auther-img" src="/images/pierogies.jpg" alt=""></img>
+
+//<CustomImage imgSrc={recipe.image} pt="65%"/>
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -2,37 +2,39 @@ import RecipeCard from "../components/recipecard"
 export default function Recipes(){
     const recipes = [
         {
-            title: "protein muffins",
-            image: "/images/proteinm3.jpg"
+            title: "oat chocolate muffins",
+            image: "/images/proteinm3.jpg",
+            info:"super easy choco muffins that i devour for breakfast"
         }, 
         {
             title: "chilli paneer",
-            image: "/images/chillipaneer.jpg"
+            image: "/images/chillipaneer.jpg",
+            info:"quick paneer because i cannot live without it"
         },
         {
             title: "quinoa salad",
-            image: "/images/quinoa.jpg"
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
+            image: "/images/quinoa.jpg",
+            info:"simple quinoa salads have saved my lunches"
         },
         {
             title: "cucumber roll",
-            image: "/images/roll.jpg"
+            image: "/images/roll.jpg",
+            info:"cute way of getting more veggies in"
         },
         {
             title: "tomato soup",
-            image: "/images/soup.jpg"
+            image: "/images/soup.jpg",
+            info:"for throw in a pot and let happen type of nights"
         }, 
         {
             title: "paneer & cabbage dumplings",
-            image: "public/images/dumplings.jpg"
+            image: "/images/dumplings.jpg",
+            info:"for when you are feeling fancy and need to feed a village"
         },
         {
             title: "black bean tacos",
-            image: "/images/taco.jpg"
+            image: "/images/taco.jpg",
+            info:"my absolute favourite tacos that my family also love"
         }
     ].sort(() => Math.random() - 0.5)
 
@@ -45,9 +47,8 @@ export default function Recipes(){
                 <button className="btn">go!</button>
             </div>
             <div className="recipes-container">
-                {/* <RecipeCard /> */}
-                {recipes.map((recipe, index) => (
-                    <RecipeCard key={index} recipe={recipe} />
+                {recipes.map((recipe) => (
+                    <RecipeCard key={recipe.title} recipe={recipe} />
                 ))}
             </div>
             
